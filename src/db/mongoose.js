d8cfaec19a153164087a08c6474e4437edf44484
Priyanshu Raj <rpriyanshu9@gmail.com>
2021-07-10 12:21:08 +0530

    completed REST API methods for CRUD operations

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose')
 const Book = require('../models/books')
 
-mongoose.connect('mongodb://127.0.0.1:27017/books-directory', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }, () => {
+mongoose.connect('mongodb://127.0.0.1:27017/books-directory', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }, () => {
     console.log("Connected to mongodb.")
 });
-
-
--- src/routes/booksRoute.js
+++ src/routes/booksRoute.js
@@ -1,4 +1,68 @@
 const express = require('express')
+const Book = require('../models/books')
 const router = express.Router()
+
+// Create new book
+router.post('/', async (req, res) => {
+    try {
+        const book = new Book(req.body)
+        await book.save()
+        res.status(201).send()
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+// Read all books
+router.get('/', async (req, res) => {
+    try {
+        const allBooks = await Book.find()
+        res.send(allBooks)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+// Read single book
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const book = await Book.findById(id)
+        if (!book) {
+            return res.status(404).send()
+        }
+        res.send(book)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+// Update book
+router.patch('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const book = await Book.findByIdAndUpdate(id, req.body, { new: true, runValidators: true })
+        if (!book) {
+            return res.status(404).send()
+        }
+        res.send(book)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+// Delete book
+router.delete('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const book = await Book.findByIdAndDelete(id)
+        if (!book) {
+            return res.status(404).send()
+        }
+        res.send(book)
+    } catch (error) {
+        res.status(500).send()
+    }
+})
 
 module.exports = router
